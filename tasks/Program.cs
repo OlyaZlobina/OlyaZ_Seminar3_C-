b1d03757bbbab84acc32b0ddfb2910993850646b
Olya <Zlobinaolya87@gmail.com>
2022-06-20 11:36:13 +0300

    done

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -1,40 +1,55 @@
 ﻿// Задача 19: *Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-// Console.WriteLine(" Input five digit number: ");
-// int fiveDNum = Convert.ToInt32(Console.ReadLine());
-// void Palindrom(){
-//     int num1 = fiveDNum / 10000 % 10;
-//     Console.Write(num1);
-//     int num2 = fiveDNum / 1000 % 10;
-//     Console.Write(num2);
-//     int num4 = fiveDNum / 10 % 10;
-//     Console.Write(num4);
-//     int num5 = fiveDNum % 10;
-//     Console.WriteLine(num5);
-//     if (num1 == num5 && num2 == num4)
-//     {
-//         Console.WriteLine(fiveDNum + "Number is a palindrom");
-//     }
-//     else
-//     {
-//         Console.WriteLine("Number is not a palindrom");
-//     }
-// }
-// Palindrom();
+Console.WriteLine(" Input five digit number: ");
+int fiveDNum = Convert.ToInt32(Console.ReadLine());
+void Palindrom(){
+    int num1 = fiveDNum / 10000 % 10;
+    Console.Write(num1);
+    int num2 = fiveDNum / 1000 % 10;
+    Console.Write(num2);
+    int num4 = fiveDNum / 10 % 10;
+    Console.Write(num4);
+    int num5 = fiveDNum % 10;
+    Console.WriteLine(num5);
+    if (num1 == num5 && num2 == num4)
+    {
+        Console.WriteLine(fiveDNum + "Number is a palindrom");
+    }
+    else
+    {
+        Console.WriteLine("Number is not a palindrom");
+    }
+}
+Palindrom();
 
 // *Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
 
-// void Distans(int Xa, int Ya, int Za, int Xb, int Yb, int Zb){
-//     double result = Math.Sqrt((Xa - Xb)*(Xa - Xb) + (Ya - Yb)*(Ya - Yb)+ (Za - Zb));
-//     Console.Write(result);
-// }
-// Console.WriteLine("Input coordinats first dot: ");
-// int Xa = Convert.ToInt32(Console.ReadLine());
-// int Ya = Convert.ToInt32(Console.ReadLine());
-// int Za = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input coordinats second dot: ");
-// int Xb = Convert.ToInt32(Console.ReadLine());
-// int Yb = Convert.ToInt32(Console.ReadLine());
-// int Zb = Convert.ToInt32(Console.ReadLine());
-// Distans(Xa, Ya, Za, Xb, Yb, Zb);
+void Distans(int Xa, int Ya, int Za, int Xb, int Yb, int Zb){
+    double result = Math.Sqrt((Xa - Xb)*(Xa - Xb) + (Ya - Yb)*(Ya - Yb)+ (Za - Zb));
+    Console.Write(result);
+}
+Console.WriteLine("Input coordinats first dot: ");
+int Xa = Convert.ToInt32(Console.ReadLine());
+int Ya = Convert.ToInt32(Console.ReadLine());
+int Za = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input coordinats second dot: ");
+int Xb = Convert.ToInt32(Console.ReadLine());
+int Yb = Convert.ToInt32(Console.ReadLine());
+int Zb = Convert.ToInt32(Console.ReadLine());
+Distans(Xa, Ya, Za, Xb, Yb, Zb);
+
+// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.WriteLine("Input number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+void cube(){
+    int num = 1;
+    while(num < N){
+        int result = num * num * num;
+        Console.Write( result + " " );
+        num++;
+    }
+}
+cube(); 
